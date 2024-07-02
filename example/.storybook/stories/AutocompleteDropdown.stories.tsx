e3ee85daaa78b76135ec9a5c78f9cf16568383f1
Abiraman K <abiramancit@gmail.com>
2024-07-02 17:56:06 +0530

    refactor: story for AutocompleteDropdown

--- example/.storybook/stories/AutocompleteDropdown.stories.tsx
+++ example/.storybook/stories/AutocompleteDropdown.stories.tsx
@@ -1,8 +1,26 @@
-import React from 'react';
+import React, { useState, type ComponentProps } from 'react';
 import type { Meta, StoryObj } from '@storybook/react';
 import { StyleSheet, View } from 'react-native';
 
-import { AutocompleteDropdown } from 'react-native-design';
+import {
+  AutocompleteDropdown,
+  DesignProvider,
+  isEmpty,
+  responsive,
+} from 'react-native-design';
+import type { AutocompleteDropdownProps } from 'react-native-design';
+
+const dataSet: Pick<AutocompleteDropdownProps, 'dataSet'>['dataSet'] = [
+  { id: '1', title: 'Cupcake' },
+  { id: '2', title: 'Donut' },
+  { id: '3', title: 'Eclair' },
+  { id: '4', title: 'Froyo' },
+  { id: '5', title: 'Gingerbread' },
+  { id: '6', title: 'Honeycomb' },
+  { id: '7', title: 'Ice Cream Sandwich' },
+];
+
+const onOpenSuggestionsList = (_: any) => {};
 
 const meta = {
   component: AutocompleteDropdown,
@@ -13,7 +31,11 @@ const meta = {
       </View>
     ),
   ],
-  args: {},
+  args: {
+    containerStyle: {
+      width: '100%',
+    },
+  },
   argTypes: {},
 } satisfies Meta<typeof AutocompleteDropdown>;
 
@@ -21,8 +43,34 @@ export default meta;
 
 type Story = StoryObj<typeof meta>;
 
-export const Default: Story = {
-  args: {},
+export const Default: StoryObj<ComponentProps<typeof AutocompleteDropdown>> = {
+  args: {
+    label: 'Android Versions',
+    placeholder: 'Enter android version',
+    emptyResultText: 'Android version not found',
+    onOpenSuggestionsList,
+  },
+  render: (args) => {
+    const [searchText, setSearchText] = useState<string>('');
+
+    const filter = dataSet.filter((item) =>
+      item.title?.toLocaleLowerCase()?.includes(searchText?.toLocaleLowerCase())
+    );
+
+    function onChangeText(text: string) {
+      setSearchText(text);
+    }
+
+    return (
+      <DesignProvider>
+        <AutocompleteDropdown
+          {...args}
+          dataSet={isEmpty(searchText) ? dataSet : filter}
+          onChangeText={onChangeText}
+        />
+      </DesignProvider>
+    );
+  },
 };
 
 const styles = StyleSheet.create({
@@ -30,5 +78,6 @@ const styles = StyleSheet.create({
     flex: 1,
     alignItems: 'center',
     justifyContent: 'center',
+    paddingHorizontal: responsive.size(10),
   },
 });
