3b920fd76c35c5d05be89d5bb4673f748c595166
Abiraman K <abiramancit@gmail.com>
2024-08-01 22:45:58 +0530

    chore: export required modules

--- src/themes/index.ts
+++ src/themes/index.ts
@@ -1,3 +1,4 @@
 export * from './appColors';
 export * from './themes';
 export * from './appTheme';
+export * from './themes';
--- src/types/index.ts
+++ src/types/index.ts
@@ -1,4 +1,4 @@
-export * from './props';
-export * from './utils';
 export * from './context';
+export * from './props';
 export * from './theme';
+export * from './utils';
